0f5bf79f371242566794b40851e7ca7c6bf72316
neight01 <shadowneight@gmail.com>
2025-02-02 21:34:06 +0100

    Create JSSpoofer.ps1

---
+++ main/JSSpoofer.ps1
@@ -0,0 +1,17 @@
+Clear-Host
+Write-Host @"
+
+   __  __   __                    __           
+   \ \/ _\ / _\_ __   ___   ___  / _| ___ _ __ 
+    \ \ \  \ \| '_ \ / _ \ / _ \| |_ / _ \ '__|
+ /\_/ /\ \ _\ \ |_) | (_) | (_) |  _|  __/ |   
+ \___/\__/ \__/ .__/ \___/ \___/|_|  \___|_|   
+              |_|                                                             
+                       
+"@ -ForegroundColor Cyan
+
+Write-Host "Made by Neight01`n"
+
+Write-Host "Press 1 to spoof EasyAdmin"
+Write-Host "Press 2 to spoof TXAdmin"
+Write-Host "Press 3 to spoof Anything"
